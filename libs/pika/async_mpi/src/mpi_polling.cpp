ece78323958bcf3ce41ed97b3402d89a3e3a1da0
John Biddiscombe <biddisco@cscs.ch>
2024-01-12 08:09:54 +0100

    Turn on max debugging messages

--- libs/pika/async_mpi/src/mpi_polling.cpp
+++ libs/pika/async_mpi/src/mpi_polling.cpp
@@ -57,7 +57,7 @@ namespace pika::mpi::experimental {
         // by convention the title is 7 chars (for alignment)
         // a debug level of N shows messages with level 1..N
         template <int Level>
-        static debug::detail::print_threshold<Level, 0> mpi_debug("MPIPOLL");
+        static debug::detail::print_threshold<Level, 9> mpi_debug("MPIPOLL");
 
         constexpr std::uint32_t max_mpi_streams = detail::to_underlying(stream_type::max_stream);
         constexpr std::uint32_t max_poll_requests = 32;
